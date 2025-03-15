44675e6d898ce77fa38c207aafdfb82a734daa98
Leutrim Haziri <leutrimhaziri@Leutrims-MacBook-Air.local>
2025-03-15 22:54:04 +0100

    Initial commit

---
+++ src/api/sherbimet/controllers/sherbimet.ts
@@ -0,0 +1,7 @@
+/**
+ * sherbimet controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::sherbimet.sherbimet');
---
+++ src/api/sherbimet/services/sherbimet.ts
@@ -0,0 +1,7 @@
+/**
+ * sherbimet service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::sherbimet.sherbimet');
