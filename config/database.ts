7010033f029fa83c30553ed32b638f94f7708f3c
Leutrim Haziri <leutrimhaziri@Leutrims-MacBook-Air.local>
2025-03-16 10:30:30 +0100

    Initial commit

--- config/database.ts
+++ config/database.ts
@@ -1,18 +1,12 @@
-    // strapi-api/config/database.js
-    module.exports = ({ env }) => ({
-      connection: {
-        client: 'postgres',
-        connection: {
-          host: env('DATABASE_HOST', ),
-          port: env.int('DATABASE_PORT', 5432),
-          database: env('DATABASE_NAME', ),
-          user: env('DATABASE_USERNAME', ),
-          password: env('DATABASE_PASSWORD', ),
-          schema: env('DATABASE_SCHEMA', 'public'), // Not required
-          ssl: {
-            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
-          },
-        },
-        debug: false,
-      },
-    });
+
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      connectionString: env('DATABASE_URL'),
+      ssl: env.bool('DATABASE_SSL', true) ? { rejectUnauthorized: false } : false,
+    },
+    debug: false,
+  },
+});
